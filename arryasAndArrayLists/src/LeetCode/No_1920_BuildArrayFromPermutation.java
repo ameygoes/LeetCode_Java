f8d8be3316671b5e8b75d774b2f16700b28acaa4
Amey <bamey2241997@gmail.com>
2022-08-23 15:23:41 -0700

    Added 1920 problem

---
+++ arryasAndArrayLists/src/LeetCode/No_1920_BuildArrayFromPermutation.java
@@ -0,0 +1,7 @@
+/*
+* Copyright (c) 8/23/2022 . Amey Bhilegaonkar. All rights reserved.
+* AUTHOR: Amey Bhilegoankar
+* PORTFOLIO: https://ameyportfolio.netlify.app/
+* FILE CREATION DATE: 8/23/2022
+*/package LeetCode;public class No_1920_BuildArrayFromPermutation {
+}
