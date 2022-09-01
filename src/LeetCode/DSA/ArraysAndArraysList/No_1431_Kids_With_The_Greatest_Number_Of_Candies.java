84ad514054b40a67add67aae2ee7e2083dc51e12
Amey <bamey2241997@gmail.com>
2022-08-31 21:02:25 -0700

    Added 1431

---
+++ src/LeetCode/DSA/ArraysAndArraysList/No_1431_Kids_With_The_Greatest_Number_Of_Candies.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 8/31/2022 . Amey Bhilegaonkar. All rights reserved.
+ * AUTHOR: Amey Bhilegoankar
+ * PORTFOLIO: https://ameyportfolio.netlify.app/
+ * FILE CREATION DATE: 8/31/2022
+ */
+package LeetCode.DSA.ArraysAndArraysList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No_1431_Kids_With_The_Greatest_Number_Of_Candies {
+    static int calculateMax(int[] candies){
+
+        int max = Integer.MIN_VALUE;
+        for (int element:candies) {
+            max = Math.max(element, max);
+        }
+        return max;
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        List<Boolean> ansList=new ArrayList<Boolean>();
+
+        int max = calculateMax(candies);
+        for (int itemIndex = 0; itemIndex < candies.length; itemIndex++) {
+            if(candies[itemIndex] + extraCandies >= max ){
+                ansList.add(Boolean.TRUE);
+            }
+            else {
+                ansList.add(Boolean.FALSE);
+            }
+
+        }
+        return ansList;
+
+    }
+
+    public static void main(String[] args) {
+        int[] candies = {4,2,1,1,2};
+        int extraCandies = 1;
+        System.out.println(kidsWithCandies(candies, extraCandies));
+    }
+}
+
